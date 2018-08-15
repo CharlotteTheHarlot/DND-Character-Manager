fa14d7e3c55a8746fef7545c91c480b71b5c2c96
Charlotte <AgentBrain804@gmail.com>
2018-08-14 22:42:47 -0400

    Added the program description.

---
+++ Main_Program.py
@@ -0,0 +1,12 @@
+'''
+This is a Dungeon's and Dragon's character manager created by none other than Charlotte!
+With this manager you will be able to:
+Create, save, and load a character.
+Keep track of stats like Con, Str, Wis, Int, Chr, and Dex.
+Keep track of inventory.
+Track useful things like current/max HP, Armor Class, Speed.
+Have a built in dice roll function.
+Have a built in currency function. (player wallet, purchases, sales, unit converter)
+Track quests, allies, enimies, and guilds.
+And as I think of more I will add it.
+'''
