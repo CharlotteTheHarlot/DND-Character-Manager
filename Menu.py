52783b332799acd437de7363d9bed558efc81543
Charlotte <AgentBrain804@gmail.com>
2018-08-14 23:34:02 -0400

    Added a menu to take the user's input and determine what the user typed.

---
+++ Menu.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+'''
+This is the menu that interpruts, translates, and outputs a usable user input. 
+'''
+
+def menu(user_input):
+    if user_input in ['Yes', 'Y', 'Confirm']:
+        return 1
+    elif user_input in ['No', 'N', 'Deny']:
+        return 2
+    elif user_input in ['Load', 'Load character']:
+        return 3
+    elif user_input in ['New', 'New character', 'Create', 'Create new', 'Create new character', 'Create character']:
+        return 4
+    elif user_input in ['Quit', 'Exit', 'Terminate', 'Terminate program']:
+        return 5
+#    elif user_input in []:
+#        return 6
+#    elif user_input in []:
+#        return 7
+#    elif user_input in []:
+#        return 8
+#    elif user_input in []:
+#        return 9
+#    elif user_input in []:
+#        return 10
+#    elif user_input in []:
+#        return 11
+#    elif user_input in []:
+#        return 12
+#    elif user_input in []:
+#        return 13
+#    elif user_input in []:
+#        return 14
+#    elif user_input in []:
+#        return 15
+#    elif user_input in []:
+#        return 16
+#    elif user_input in []:
+#        return 17
+#    elif user_input in []:
+#        return 18
+#    elif user_input in []:
+#        return 19
+    else:
+        return 0#This means the input was not recognized.
