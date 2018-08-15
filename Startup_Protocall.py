d35ef8cd0f51608976a65bd564bc366bcb729408
Charlotte <AgentBrain804@gmail.com>
2018-08-14 23:01:20 -0400

    Added a simple print thingy to show that the program is running.

--- Startup_Protocall.py
+++ Startup_Protocall.py
@@ -4,3 +4,12 @@ It will run on startup. (Hence the obvious name)
 It runs through a basic greetings.
 If for whatever reason I need to load functions/other stuff I an put that here too.
 '''
+
+def startup_protocall():
+    print("Starting D&D Character Manager...")
+    print("Loading...")
+    print("Almost done...")
+    print("Oh, wait...nevermind...")
+    print("Come on! It was a joke... ")
+    print("Fine...Loading done!")
+    print("Welcome! This is the D&D Character Manager! It was created by the very pretty Charlotte!")
