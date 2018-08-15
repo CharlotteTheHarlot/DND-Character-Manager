9a62c0886d33aadffc85c387a5430d24abe94faa
Charlotte <AgentBrain804@gmail.com>
2018-08-14 22:50:20 -0400

    Changed the name of 'Startup' to be 'Startup_Protocall' and added a description.

---
+++ Startup_Protocall.py
@@ -0,0 +1,6 @@
+'''
+This is the startup protocall.
+It will run on startup. (Hence the obvious name)
+It runs through a basic greetings.
+If for whatever reason I need to load functions/other stuff I an put that here too.
+'''
