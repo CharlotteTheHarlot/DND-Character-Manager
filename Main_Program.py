60b4c76c6d958642da8f8a77414f2ba6b80cf9c8
Charlotte <AgentBrain804@gmail.com>
2018-08-15 00:00:19 -0400

    Added a main menu. There are currently 3 known buggs. First 2 are the character_creator and the load_character commands. I haven't made the corresponding programs they call on yet. The last bugg is dealing with the "user_command = imput()" statement. It doesn't feed the result through the Menu function to return the corresponding numbers.

--- Main_Program.py
+++ Main_Program.py
@@ -10,3 +10,21 @@ Have a built in currency function. (player wallet, purchases, sales, unit conver
 Track quests, allies, enimies, and guilds.
 And as I think of more I will add it.
 '''
+
+from Startup_Protocall import startup_protocall
+
+startup_protocall()
+while True:
+    print("--------Main Menu--------")
+    print("Load Existing Character: ")
+    print("Create a New Character: ")
+    print("Quit the Program: ")
+    user_command = input("What would you like to do? > ")
+    if user_command == 3:#This means load character.
+        print("Ok! Starting up character creator program!")
+        character_creator()
+    elif user_command == 4:#This means create character.
+        print("Ok! Starting up character load program!")
+        load_character()
+    else:
+        print("I'm sorry. I didn't understand what you just said. Please try again.")
